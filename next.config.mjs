5d3749fcdd8b1eada890ea33962cfdc7f73ace20
ohsssuk <ohsssuk@naver.com>
2024-03-12 00:35:58 +0900

    세팅 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repository = 'http://ohsssuk.github.io/dho-cal';
 
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? `/${repository}/` : '', // production 일때 prefix 경로
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  output: 'export',
 };
 
 export default nextConfig;
