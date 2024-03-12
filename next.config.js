64430291f0e03f43b347ba1407a406aaae0650f2
ohsssuk <53249002+ohsssuk@users.noreply.github.com>
2024-03-12 14:09:05 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@
 const nextConfig = {
   output: 'export',
 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  async rewrites() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: 'https://ohsssuk.github.io/dho-cal/favicon.ico',
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
