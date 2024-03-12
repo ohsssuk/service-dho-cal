2f3405655def5c0d18aef32a93630e65456c4f9f
ohsssuk <53249002+ohsssuk@users.noreply.github.com>
2024-03-12 14:11:37 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@
  */
 const nextConfig = {
   output: 'export',
-
-  async rewrites() {
-    return [
-      {
-        source: '/favicon.ico',
-        destination: 'https://ohsssuk.github.io/dho-cal/favicon.ico',
-      },
-    ]
-  },
 };
 
 module.exports = nextConfig;
